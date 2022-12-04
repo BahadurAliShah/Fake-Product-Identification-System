2eb793698d6555fa9c62d5a7d9eb6dadcd39a082
BahadurAliShah <sayyedbahadur@gmail.com>
2022-12-04 19:44:04 +0500

    Tailwind cdn added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Admin from "./pages/Admin";
-import './assets/tailwind/main.css'
+// import './assets/tailwind/main.css'
 import './assets/App.css'
 
 function App() {
